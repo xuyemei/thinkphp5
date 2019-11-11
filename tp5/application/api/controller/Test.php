32540550b260d75dc42fe120ab4353f0f94de307
1454346617 <1454346617@qq.com>
2019-11-11 15:39:30 +0800

    '重写框架的异常处理类，适用于api的错误处理返回'

--- tp5/application/api/controller/Test.php
+++ tp5/application/api/controller/Test.php
@@ -13,6 +13,7 @@ class Test extends Controller
         if($data['mt'] != 1){
             exception('数据不合法哦');
             throw new ApiException('数据不合法哦',403);
+
         }
 
         return show(1,'ok',$data,201);
